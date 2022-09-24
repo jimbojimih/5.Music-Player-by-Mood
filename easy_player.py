9c8df2a09de737509f38129d642e992da0dd6084
Dmitry Oparin <jim_hendrix@mail.ru>
2022-09-24 16:57:15 +0500

    PyQt6 start

--- easy_player.py
+++ easy_player.py
@@ -1,37 +1,84 @@
-from ytmusicapi import YTMusic
-from moviepy.editor import AudioFileClip
 import os
+import time
+from tkinter import *
+
+from ytmusicapi import YTMusic
 import pafy
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 basepath = os.path.dirname(os.path.abspath(__file__))
 dllspath = os.path.join(basepath, 'dlls')
 os.environ['PATH'] = dllspath + os.pathsep + os.environ['PATH']
+
 import mpv
 
-mood_dict = {}
+
 ytmusic = YTMusic()
 ytmusic.setup
-dict_ = ytmusic.get_mood_categories()
-'''
-for a in dict_['Moods & moments']:
-    mood_dict[a['title']] = a['params']
-#print(mood_dict)
-'''
+get_raw_dict = ytmusic.get_mood_categories()
+dict_mood_and_parametrs = {}
 
-#print(ytmusic.get_mood_playlists(mood_dict['Chill'])[0])
+for a in get_raw_dict['Moods & moments']:
+    dict_mood_and_parametrs[a['title']] = a['params']
 
-#print(ytmusic.get_playlist('RDCLAK5uy_lFLL3kvPVwDnLJYBQuRmtP3-1_w63yUSk')['tracks'])
 
-video_url = "https://www.youtube.com/watch?v=" + 'e4PBKgLlaHk'
+first_chill_playlist = ytmusic.get_mood_playlists(dict_mood_and_parametrs['Chill'])[0]
+
+playlist_id = first_chill_playlist['playlistId']
+
+track_list_from_playlist = ytmusic.get_playlist(playlist_id)['tracks']
+
+video_id = track_list_from_playlist[0]['videoId']
+
+video_url = "https://www.youtube.com/watch?v=" + video_id
 video = pafy.new(video_url)
 audio = video.getbestaudio()
 audio_url = audio.url
 
+player = mpv.MPV(ytdl=True) #ytdl=True
+player.playlist_append(audio_url)
+#print(player.volume)
+#self.mpv_player.playlist_clear()
+def pause():
+    player.pause = True
+def resume():
+    player.pause = False
+def play():   
+    player.playlist_pos = 0
 
-#clip = AudioFileClip(audio_url)
-#clip.preview(fps=44100)
+              
+root = Tk()
+root.geometry('600x300')    
+Label(root, text = 'g', font='lucidia 30 bold').pack()
+Button(text='play', command=play).place(x=200, y=100)
+Button(text='pause', command=pause).place(x=250, y=100)
+Button(text='resume', command=resume).place(x=150, y=100)
+root.mainloop()
 
 
-player = mpv.MPV(ytdl=True)
-player.play(audio_url)
-player.wait_for_playback()
+'''
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+
+        # Устанавливаем центральный виджет Window.
+        self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
+'''
