5da9196952e4cae1e93c6b7549c62df8f709c83d
Dmitry Oparin <jim_hendrix@mail.ru>
2022-10-19 18:14:51 +0500

    refract done

--- easy_player.py
+++ easy_player.py
@@ -5,23 +5,14 @@ from ytmusicapi import YTMusic
 from PyQt6.QtGui import QIcon, QFont
 from PyQt6.QtCore import QSize, Qt, QTimer, QThread
 from PyQt6.QtWidgets import (  
-        QPushButton,
-        QMainWindow,
-        QApplication,
-        QVBoxLayout,
-        QHBoxLayout,
-        QWidget,
-        QComboBox,
-        QSlider,
-        QLabel,
-        QMenuBar,
-        QMenu,
-        QMessageBox
+        QPushButton, QMainWindow, QApplication, QVBoxLayout, QHBoxLayout,
+        QWidget, QComboBox, QSlider, QLabel, QMenuBar, QMenu, QMessageBox,
 )
 
 basepath = os.path.dirname(os.path.abspath(__file__))
 dllspath = os.path.join(basepath, 'dlls')
 os.environ['PATH'] = dllspath + os.pathsep + os.environ['PATH']
+
 import mpv
 
 
@@ -29,7 +20,9 @@ class PlayListCreator(QThread):
     '''Get the url of the audio to be played by the player.'''
     def __init__(self):
         QMainWindow.__init__(self)
+
         self.play_lists_json = {}
+
         self.ytmusic = YTMusic()         
         self.ytmusic.setup
 
@@ -43,10 +36,11 @@ class PlayListCreator(QThread):
         '''Get a dictionary with mood playlists.'''
         self.create_mood_param_dict()
         for mood, params in self.play_lists_json.items():
-            self.play_lists_json[mood] = self.ytmusic.get_mood_playlists(params)        
+            self.play_lists_json[mood] = self.ytmusic.get_mood_playlists(
+                    params)        
 
     def create_clean_mood_playlists_id_dict(self):
-        '''Get a dictionary with clean mood playlists. '''
+        '''Get a dictionary with clean mood playlists.'''
         self.create_mood_playlists_id_dict()
         for mood, params in self.play_lists_json.items(): 
             list_playlist_id = []   
@@ -61,7 +55,8 @@ class PlayListCreator(QThread):
             list_all_video_id = []
             for param in params:
                 tracks_from_playlist = []
-                tracks_from_playlist_raw = self.ytmusic.get_playlist(param)['tracks']
+                tracks_from_playlist_raw = self.ytmusic.get_playlist(
+                        param)['tracks']
                 for track in tracks_from_playlist_raw:
                     video_id = track['videoId']
                     if video_id is not None:
@@ -104,15 +99,13 @@ class PlaybackControl():
 
         current_playlist = self.play_lists_json[self.mood][self.numb_playlist]       
 
-        for video_id in current_playlist:
-            link = "https://www.youtube.com/watch?v=" + video_id
-            self.player.playlist_append(link)
-
+        self.playlist_append(current_playlist)
         self.player.playlist_pos = pos_track
 
     def update_last_tracks_played(self):
         '''Update in GUI'''
-        self.last_tracks[self.mood] = [self.player.playlist_pos, self.numb_playlist]
+        self.last_tracks[self.mood] = [self.player.playlist_pos, 
+                                       self.numb_playlist]
 
     def turn_on_the_next_playlist(self):
         len_playlist_of_mood = len(self.play_lists_json[self.mood]) - 1
@@ -120,28 +113,33 @@ class PlaybackControl():
             self.player.stop()
             self.player.playlist_clear()
             self.numb_playlist += 1
-            current_playlist = self.play_lists_json[self.mood][self.numb_playlist]
 
-            for video_id in current_playlist:
-                link = "https://www.youtube.com/watch?v=" + video_id
-                self.player.playlist_append(link)
+            current_playlist = self.play_lists_json[
+                    self.mood][self.numb_playlist]
 
+            self.playlist_append(current_playlist)
             self.player.playlist_pos = 0
-        else: pass
+        else: 
+            pass
 
     def turn_on_the_previous_playlist(self):
         if self.numb_playlist > 0:
             self.player.stop()
             self.player.playlist_clear()
             self.numb_playlist -= 1
-            current_playlist = self.play_lists_json[self.mood][self.numb_playlist]
 
-            for video_id in current_playlist:
-                link = "https://www.youtube.com/watch?v=" + video_id
-                self.player.playlist_append(link)
+            current_playlist = self.play_lists_json[self.mood][
+                    self.numb_playlist]
 
+            self.playlist_append(current_playlist)
             self.player.playlist_pos = 0
-        else: pass
+        else: 
+            pass
+
+    def playlist_append(self, current_playlist):
+        for video_id in current_playlist:
+            url = "https://www.youtube.com/watch?v=" + video_id
+            self.player.playlist_append(url)
 
     def get_video_id(self):
         return self.player.filename
@@ -151,7 +149,10 @@ class PlaybackControl():
             self.turn_on_the_next_playlist()  
 
     def reset(self):
-        self.last_tracks = {"Chill": [0, 0], "Commute": [0, 0], "Energy Boosters": [0, 0], "Feel Good": [0, 0], "Focus": [0, 0], "Party": [0, 0], "Romance": [0, 0], "Sleep": [0, 0], "Workout": [0, 0]}
+        self.last_tracks = {
+                "Chill": [0, 0], "Commute": [0, 0], "Energy Boosters": [0, 0],
+                "Feel Good": [0, 0], "Focus": [0, 0], "Party": [0, 0], 
+                "Romance": [0, 0], "Sleep": [0, 0], "Workout": [0, 0]}
         self.set_mood(self.mood)
         
     def save_the_last_track(self):
@@ -170,25 +171,30 @@ class LastTracksPlayed():
             last_track = json.load(file)
         return last_track
 
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Easy Player")
+        self.setFixedWidth(300)
+        self.setFixedHeight(130)
+        self.setWindowIcon(QIcon('icon.png')) 
+
+        self.init_ui()
+
+        self.show()
+
+    def init_ui(self):
         self.playback_control = PlaybackControl()
         self.playback_control.set_mood('Chill')
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.set_title)
-        self.timer.timeout.connect(lambda: self.playback_control.update_last_tracks_played())
-        self.timer.timeout.connect(lambda: self.playback_control.check_the_last_track())
-        self.timer.start(500)
-        
-        self.play_list_creator = PlayListCreator()
+        self.menu = QMenuBar()   
 
-        self.menu = QMenuBar()         
         self.file_menu = QMenu('File')
         self.menu.addMenu(self.file_menu)
         self.file_menu.addAction('Update playlist', self.update_play_list)
-        self.file_menu.addAction('Reset the save of lasts tracks played', lambda: self.playback_control.reset())
+        self.file_menu.addAction('Reset the save of lasts tracks played', 
+                                  lambda: self.playback_control.reset())
 
         self.menu.addAction('About Easy Player', self.show_about_app_window)              
         
@@ -199,11 +205,13 @@ class MainWindow(QWidget):
 
         self.button_return = QPushButton("<--")
         self.button_return.setFixedSize(85, 30) 
-        self.button_return.clicked.connect(lambda: self.playback_control.turn_on_the_previous_playlist())
+        self.button_return.clicked.connect(
+                lambda: self.playback_control.turn_on_the_previous_playlist())
 
         self.next_playlist = QPushButton("-->") 
         self.next_playlist.setFixedSize(85, 30) 
-        self.next_playlist.clicked.connect(lambda: self.playback_control.turn_on_the_next_playlist())
+        self.next_playlist.clicked.connect(
+                lambda: self.playback_control.turn_on_the_next_playlist())
 
         self.slider = QSlider(Qt.Orientation.Horizontal)
         self.slider.setRange(0, 100)
@@ -212,42 +220,41 @@ class MainWindow(QWidget):
         self.slider.valueChanged.connect(self.volume)
 
         self.choice = QComboBox()
-
         self.choice.setFixedSize(100, 30)
         self.choice.addItems(["Chill", "Commute", "Energy Boosters"])
         self.choice.addItems(["Feel Good", "Focus", "Party"])
         self.choice.addItems(["Romance", "Sleep", "Workout"])
         self.choice.currentTextChanged.connect(self.set_mood)
 
-        self.title = QLabel()
-        self.title.setOpenExternalLinks(True)
+        self.link = QLabel()
+        self.link.setOpenExternalLinks(True)
         
         layout1 = QHBoxLayout()  
-
         layout1.addWidget(self.button_return)
         layout1.addWidget(self.button)
         layout1.addWidget(self.next_playlist)
         
         layout2 = QHBoxLayout()
-
         layout2.addWidget(self.slider)        
         layout2.addWidget(self.choice)
 
         layout3 = QVBoxLayout()  
-
         layout3.setMenuBar(self.menu)
         layout3.addLayout(layout1)  
         layout3.addStretch(1) 
         layout3.addLayout(layout2) 
         layout3.addStretch(1)                            
-        layout3.addWidget(self.title) 
+        layout3.addWidget(self.link) 
 
         self.setLayout(layout3)
-        self.setWindowTitle("Easy Player")
-        self.setFixedWidth(300)
-        self.setFixedHeight(130)
-        self.setWindowIcon(QIcon('icon.png'))
-        self.show()
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.set_link)
+        self.timer.timeout.connect(
+                lambda: self.playback_control.update_last_tracks_played())
+        self.timer.timeout.connect(
+                lambda: self.playback_control.check_the_last_track())
+        self.timer.start(500)
 
     def play(self, checked):
         self.playback_control.play(checked)
@@ -261,46 +268,59 @@ class MainWindow(QWidget):
     def set_mood(self, currentText):
         self.playback_control.set_mood(currentText)  
 
-    def set_title(self):
+    def set_link(self):
         video_id = self.playback_control.get_video_id()
 
         if self.playback_control.get_video_id() is not None:
             url_text = 'https://www.youtube.com/' + video_id
-            url = "<a href=\"{url_text}\"> <font color=black>Open on YouTube</a>".format(url_text=url_text)
-            self.title.setText(url)
+            url = ("<a href=\"{url_text}\"> <font color=black>"
+                   "Open on YouTube</a>").format(url_text=url_text)
+            self.link.setText(url)
         else:
-            self.title.setText('wait')
+            self.link.setText('wait')
 
     def show_about_app_window(self):
         text_title = 'About Easy Player'
         text = ('A simple music player to listen to music in the background,' 
-                  'depending on your mood. Work is allowed on the playlists of ' 
-                  'YouTube Music. If the music is not suitable for you, you can ' 
-                  'switch the playlist. To do this, press --> or <-- '
-                  'When the program is restored again, playback will start from ' 
-                  'the last played track. To update playlists, ' 
-                  'click "Update Audio Sources" \n\n'
-                  'Простой музыкальный плеер для фонового прослушивания ' 
-                  'музыки, в зависимости от выбранного настроения. Работа '
-                  'программы основана на плейлистах YouTube Music. Если ' 
-                  'музыка не подходит для Вас, вы можете переключить плейлист. '
-                  'Для этого нажмите --> или <--. При повторном '
-                  'запуске программы, вопроизведение начнётся с последнего '
-                  'воспоизведённого трека. Для обновления плейлистов '
-                  'нажмите "Update audio sources"')
+                'depending on your mood. Work is allowed on the playlists ' 
+                'of YouTube Music. If the music is not suitable for you, ' 
+                'you can switch the playlist. To do this, press --> or <-- '
+                'When the program is restored again, playback will start '
+                'from the last played track. To update playlists, ' 
+                'click "Update Audio Sources" \n\n'
+                'Простой музыкальный плеер для фонового прослушивания ' 
+                'музыки, в зависимости от выбранного настроения. Работа '
+                'программы основана на плейлистах YouTube Music. Если ' 
+                'музыка не подходит для Вас, вы можете переключить '
+                'плейлист. Для этого нажмите --> или <--. При повторном '
+                'запуске программы, вопроизведение начнётся с последнего '
+                'воспоизведённого трека. Для обновления плейлистов '
+                'нажмите "Update audio sources"')
+
         QMessageBox.information(self, text_title, text)
 
     def update_play_list(self):  
         self.update_play_list = PlayListCreator()
 
         text_title_finish_message = 'Done'
-        text_finish_message = 'Playlists have been updated. You can reset the saving of the last played tracks. Playback will start with the updated playlists.'
-        self.update_play_list.finished.connect(lambda: QMessageBox.information(self, text_title_finish_message, text_finish_message))
+        text_finish_message = (
+                'Playlists have been updated. You can reset the saving of the '
+                'last played tracks. Playback will start with the updated '
+                'playlists.')
+
+        #multithreading
+        self.update_play_list.finished.connect(
+                lambda: QMessageBox.information(
+                    self, text_title_finish_message, text_finish_message))
         self.update_play_list.start()
 
         text_title_warning_message = 'Update'
-        text_warning_message = 'Please wait about 10 minutes. You can continue to use the player. The update takes place in the background. When the update is completed, you will see a notification.' 
-        QMessageBox.warning(self, text_title_warning_message, text_warning_message)
+        text_warning_message = (
+                'Please wait about 10 minutes. You can continue to use the '
+                'player. The update takes place in the background. When the '
+                'update is completed, you will see a notification.') 
+        QMessageBox.warning(self, text_title_warning_message, 
+                text_warning_message)
  
     def closeEvent(self, event):
         self.playback_control.save_the_last_track()
@@ -308,10 +328,7 @@ class MainWindow(QWidget):
 
 if __name__ == '__main__':  
 
-
     app = QApplication([])
-
     window = MainWindow()
     window.show()
-
     app.exec()
